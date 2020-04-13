93ad9847bfa9c9122f70c1ac954a149c08dd30dc
Jose Cuevas <josecueram@gmail.com>
2020-04-13 18:57:48 -0500

    created header componente

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+const header = () => {
+  const view = `
+    <div class="header-main">
+      <div class="header-logo">
+        <h1><a href="#">SPA</a></h1>
+      </div>
+      <nav class="header-nav">
+        <a href="#">About</a>
+      </nav>
+    </div>
+  `
+
+  return view
+}
+
+export default header

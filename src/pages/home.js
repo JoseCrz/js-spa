09c54ce105089d1f1aa794862347222598a5f0e1
Jose Cuevas <josecueram@gmail.com>
2020-04-13 18:40:19 -0500

    added home template

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+const home = () => {
+  const view = `
+    <div class="character">
+      <article class="character-item">
+        <a href="#/1/">
+        <img src="/imagen.png" alt="name">
+        <h2>Name</h2>
+        </a>
+      </article>
+    </div>
+  `
+  return view
+}
+
+export default home

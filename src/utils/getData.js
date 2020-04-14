67c00b054506961ef9a13853e15ce37559c0c68f
Jose Cuevas <josecueram@gmail.com>
2020-04-13 21:21:17 -0500

    created getData util

---
+++ src/utils/getData.js
@@ -0,0 +1,14 @@
+const URL = 'https://rickandmortyapi.com/api/'
+
+const getData = async id => {
+  const apiURL = id ? `${URL}${id}` : URL
+  try {
+    const response = await fetch(apiURL)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Something went wrong', error)
+  }
+}
+
+export default getData

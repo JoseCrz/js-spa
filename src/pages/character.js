8ba05b315888a3fdca3402b04df866a5547b48d9
Jose Cuevas <josecueram@gmail.com>
2020-04-13 21:52:44 -0500

    implemented getHash and getData in characters page

--- src/pages/character.js
+++ src/pages/character.js
@@ -1,17 +1,23 @@
-const character = () => {
+import getData from '../utils/getData'
+import getHash from '../utils/getHash'
+
+const character = async () => {
+  const id = getHash()
+  const character = await getData(id)
+
   const view = `
     <div class="characters-inner">
       <article class="characters-card">
-        <img src="" alt="">
-        <h2></h2>
+        <img src="${character.image}" alt="${character.name}">
+        <h2>${character.name}</h2>
       </article>
       <article class="characters-card">
-        <h3>Episodes:</h3>
-        <h3>Status:</h3>
-        <h3>Species:</h3>
-        <h3>Gender:</h3>
-        <h3>Origin:</h3>
-        <h3>Last known location:</h3>
+        <h3>Episodes: ${character.episode.length}</h3>
+        <h3>Status: ${character.status}</h3>
+        <h3>Species: ${character.species}</h3>
+        <h3>Gender: ${character.gender}</h3>
+        <h3>Origin: ${character.origin.name}</h3>
+        <h3>Last known location:${character.location.name}</h3>
       </article>
     </div>
   `

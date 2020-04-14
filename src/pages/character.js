efef7fc23fe28067d8e0edab0dd0368275f7d55a
Jose Cuevas <josecueram@gmail.com>
2020-04-13 19:06:42 -0500

    created character template

---
+++ src/pages/character.js
@@ -0,0 +1,22 @@
+const character = () => {
+  const view = `
+    <div class="characters-inner">
+      <article class="characters-card">
+        <img src="" alt="">
+        <h2></h2>
+      </article>
+      <article class="characters-card">
+        <h3>Episodes:</h3>
+        <h3>Status:</h3>
+        <h3>Species:</h3>
+        <h3>Gender:</h3>
+        <h3>Origin:</h3>
+        <h3>Last known location:</h3>
+      </article>
+    </div>
+  `
+
+  return view
+}
+
+export default character
